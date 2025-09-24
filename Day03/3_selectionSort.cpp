7b98e166b92af0320a5f7285a55fc4d46978bde3
Rishav <rishav870935@gmail.com>
2025-09-24 11:13:09 +0530

    new

--- Day03/3_selectionSort.cpp
+++ Day03/3_selectionSort.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"enter the size of the array: ";
-    cin>>n;
+    cout << "enter the size of the array: ";
+    cin >> n;
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
     }
-    for(int i=1;i<n;i++){
-        int mei=i-1;
-        for(int j=i-1;j<n;j++){
-            if(arr[mei]>arr[j]){
-                mei=j;
+    for(int i = 1; i < n; i++){
+        int mei = i - 1;
+        for(int j = i; j < n; j++){   
+            if(arr[mei] > arr[j]){
+                mei = j;
             }
         }
-        swap(arr[mei],arr[i-1]);
-
+        swap(arr[mei], arr[i - 1]);
     }
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
     }
+    return 0;
 }
